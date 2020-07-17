2eb4fe0336645df7a8b3880f62415468403e79ba
(rowaasr13) Oleg V. Volkov <rowaasr13@gmail.com>
2020-07-17 20:14:37 +0300

    ed2k link generator for anidb individual file page

---
+++ browser/anidb.net.file.js
@@ -0,0 +1,30 @@
+// https://anidb.net/file/(?:\d+)
+
+(() => {
+
+const selectors = {
+    size     : "tr.size>td.value",
+    ed2k     : "tr.ed2k>td.value",
+    extension: "tr.type>td.value",
+    anime    : "tr.relation>td.value>a[href*='/anime/'",
+    episode  : "tr.relation>td.value>a[href*='/episode/'",
+}
+
+let val = {}
+Object.keys(selectors).forEach((key) => {
+    let found = document.querySelector(selectors[key])
+    if (!found) { throw `Not found ${key} with selector ${selectors[key]}` }
+    val[key] = found.textContent
+})
+
+let size = val.size.match(/^([\d.]+)/)
+val.size = size[1].replace(/\D/g, '') | 0
+
+let extension = val.extension.match(/^video file, (.+)$/)
+if (!extension) { throw "Can't get extension from type in data table" }
+val.extension = extension[1]
+
+let filename = encodeURIComponent(`${val.anime} - ${val.episode}.${val.extension}`)
+return `ed2k://|file|${filename}|${val.size}|${val.ed2k}|/`
+
+})()
