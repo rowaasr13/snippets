112104940108e1cb4c413178c50b7aae5a340298
(rowaasr13) Oleg V. Volkov <rowaasr13@gmail.com>
2020-12-22 04:43:31 +0300

    pack main action into function and insure it starts after DOM is loaded

--- browser/wowhead - UI cleanup.cjs
+++ browser/wowhead - UI cleanup.cjs
@@ -23,30 +23,37 @@
         }
     }
 
-    let target = document.querySelector("#sidebar-zamaf-bt-ph")
-    target && target.remove()
+    function main() {
+        let target = document.querySelector("#sidebar-zamaf-bt-ph")
+        target && target.remove()
 
-    target = document.querySelector("#video-pos-static")
-    target && target.remove()
+        target = document.querySelector("#video-pos-static")
+        target && target.remove()
 
-    document.querySelectorAll(".zamaf-bt").forEach(target => {
-        target.childElementCount === 0 && target.remove()
-    })
+        document.querySelectorAll(".zamaf-bt").forEach(target => {
+            target.childElementCount === 0 && target.remove()
+        })
 
-    find_and_climb_to_parent('#ad-horizontal', '.blocks')
-    find_and_climb_to_parent('#ad-vertical', '.sidebar-wrapper')
+        find_and_climb_to_parent('#ad-horizontal', '.blocks')
+        find_and_climb_to_parent('#ad-vertical', '.sidebar-wrapper')
 
-    target = document.querySelector("#page-content")
-    if (target) { target.style.paddingRight = 'unset' }
+        target = document.querySelector("#page-content")
+        if (target) { target.style.paddingRight = 'unset' }
 
-    if (document.body.getAttribute("data-fullscreen-mode") != "true") {
-        target = document.querySelector('.header-user-fullscreen')
+        if (document.body.getAttribute("data-fullscreen-mode") != "true") {
+            target = document.querySelector('.header-user-fullscreen')
+            target && target.click()
+        }
+
+        target = document.querySelector(".header-expand-site")
         target && target.click()
+
+        document.querySelectorAll("[id$='ad-manager']").forEach(target => target.remove())
     }
 
-    target = document.querySelector(".header-expand-site")
-    target && target.click()
-
-    document.querySelectorAll("[id$='ad-manager']").forEach(target => target.remove())
-
+    if (document && (document.readyState === 'interactive' || document.readyState === 'complete')) {
+        return main()
+    } else {
+        window.addEventListener("DOMContentLoaded", main)
+    }
 })();
