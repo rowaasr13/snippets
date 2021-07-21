dfa713cc3adddc8562a6d4b31bcab7e4987392d3
(rowaasr13) Oleg V. Volkov <rowaasr13@gmail.com>
2021-07-22 02:32:32 +0300

    move https requests and reddit request to separate functions; prepare for adding discord

--- reddit_idlechampions_get_last_loot.js
+++ reddit_idlechampions_get_last_loot.js
@@ -1,7 +1,10 @@
 // not a module format, so you can run it in online compilers
 "use strict";
 
+const url_reddit_search_loot = 'https://www.reddit.com/r/idlechampions/search.json?q=flair_name%3A%22loot%22&restrict_sr=1&sort=new'
 const code_one_letter_range = '0-9A-Za-z!*&^%#@'
+
+const https = require('https')
 
 function get_code_patterns() {
     const code_one_letter_rx = '[' + code_one_letter_range + ']'
@@ -27,36 +30,50 @@ function get_code_patterns() {
     return all_patterns
 }
 
+function https_request_add_promise(url, options, resolve, reject) {
+    let request = https.get(url, options, (res) => {
+        let data = '';
+
+        res.on('data', (chunk) => data += chunk)
+
+        res.on('end', () => {
+            res.data = data
+            resolve(res)
+        })
+    }).on('error', (err) => reject(err))
+
+    return request
+}
+
+async function get_reddit_posts() {
+    const request = new Promise((resolve, reject) => https_request_add_promise(
+        url_reddit_search_loot,
+        {},
+        resolve, reject
+    ))
+    const res = request.then(res => {
+        const obj = JSON.parse(res.data)
+        return obj
+    })
+    return res
+}
+
 async function main() {
-    const https = require('https')
-
-    const url_search_loot = 'https://www.reddit.com/r/idlechampions/search.json?q=flair_name%3A%22loot%22&restrict_sr=1&sort=new'
-
     const all_patterns = get_code_patterns()
 
-    function https_get_promise(url, resolve, reject) {
-        return https.get(url_search_loot, (res) => {
-            let data = '';
+    let reddit_obj, discord_obj
 
-            res.on('data', (chunk) => data += chunk)
+    await Promise.all(
+        [ get_reddit_posts() ]
+    ).then(res => {
+        [ reddit_obj ] = res
+    })
 
-            res.on('end', () => {
-                res.data = data
-                resolve(res)
-            })
-
-        }).on('error', (err) => reject(err))
-    }
-
-
-    let request = new Promise((resolve, reject) => https_get_promise(url_search_loot, resolve, reject))
-    let res = await request
-    let res_obj = JSON.parse(res.data)
-
-    // console.log(res_obj.data.children)
+    // console.log(reddit_obj)
+    // console.log(reddit_obj.data.children)
 
     let errors = []
-    res_obj.data.children.forEach((post, idx) => {
+    reddit_obj.data.children.forEach((post, idx) => {
         // console.log(post.data.selftext)
 
         let match = post.data.selftext.match(all_patterns)
